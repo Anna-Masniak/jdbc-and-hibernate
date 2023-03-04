ada913ee13f7d06b018a1e6386dcd346cc92e31e
Ania <anna.masniak12@gmail.com>
2023-03-04 13:33:07 +0100

    [change] - interface with implementation- polymorphism

--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -2,6 +2,11 @@ package pl.sda.orange2.lambda;
 
 public class LambdaExercises {
     public static void main(String[] args) {
+        BoysDontCry boy = new ChlopakiNiePlacza();
+        boy.silnoreki();
+       // boy.sing();
 
+        ChlopakiNiePlacza boy2 = new ChlopakiNiePlacza();
+        boy2.sing();
     }
 }
